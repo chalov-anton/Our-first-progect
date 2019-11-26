2c60d70c22cf053b9bacae6ee4f3740650e888a8
chalov-anton <chalov.anton@gmail.com>
2019-11-26 16:45:36 +0200

    Index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', 'root', 'usersdb');
+
+if(!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+
+$sql = 'SELECT title, interests, id FROM userstable';
+$result = mysqli_query($conn, $sql);
+$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+mysqli_free_result($result);
+mysqli_close($conn);
+
+?>
+
+<!DOCTYPE html>
+    <html>
+    
+        <?php include('templates/header.php')?>
+        
+        <h4>Users</h4>
+        
+        <div>
+            <div>
+                <?php foreach($users as $user) ?>
+                <div>
+                    <div>
+                        <div>
+                            <h6><?php echo htmlspecialchars($user['title']); ?></h6>
+                            <div><?php echo htmlspecialchars($user['interests']);?></div>
+                        </div>
+                        <div>
+                            <a href="#">more info</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+        <?php include('templates/footer.php')?>
+               
+    </html>
+
+
+
