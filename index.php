5276696193c4e4725770420bf6a925e510ff8062
chalov <chalov.anton@gmail.com>
2019-12-02 17:02:19 +0200

    styles.css added for Homepage (index.php) and Add page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $conn = mysqli_connect('localhost', 'root', 'root', 'usersdb');
 
-if(!$conn) {
+if (!$conn) {
     echo 'Connection error: ' . mysqli_connect_error();
 }
 
@@ -16,33 +16,39 @@ mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
-    <html>
-    
-        <?php include('templates/header.php')?>
-        
-        <h4>Users</h4>
-        
+<html>
+<head>
+    <link href="styles.css" type="text/css" rel="stylesheet">
+</head>
+>
+
+<?php include('templates/header.php') ?>
+
+<h4>Users</h4>
+
+<div>
+    <div>
+        <?php foreach ($users
+
+        as $user) ?>
         <div>
             <div>
-                <?php foreach($users as $user) ?>
                 <div>
-                    <div>
-                        <div>
-                            <h6><?php echo htmlspecialchars($user['title']); ?></h6>
-                            <div><?php echo htmlspecialchars($user['interests']);?></div>
-                        </div>
-                        <div>
-                            <a href="#">more info test</a>
-                        </div>
-                    </div>
+                    <h6><?php echo htmlspecialchars($user['title']); ?></h6>
+                    <div><?php echo htmlspecialchars($user['interests']); ?></div>
+                </div>
+                <div>
+                    <a href="#">more info test</a>
                 </div>
             </div>
         </div>
-        
-        
-        <?php include('templates/footer.php')?>
-               
-    </html>
+    </div>
+</div>
+
+
+<?php include('templates/footer.php') ?>
+
+</html>
 
 
 
--- templates/header.php
+++ templates/header.php
@@ -1,15 +1,15 @@
 <head>
     <title></title>
-    
+
 </head>
 <body>
-    <nav >
-        <div>
-            <a href="index.php">Home Page</a>
-            <ul>
-                <li><a href="/add.php">Add</a></li>
-            </ul>
-        </div>
-    </nav>
+<nav>
+    <div>
+        <a href="index.php">Home Page</a>
+        <ul>
+            <li><a href="/add.php">Add</a></li>
+        </ul>
+    </div>
+</nav>
     
 
