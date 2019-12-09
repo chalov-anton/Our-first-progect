afaadb42b6bc05985ff62b74e0c40ae91f01edc2
chalov <chalov.anton@gmail.com>
2019-12-09 18:25:05 +0200

    added working Delete button on Details page

--- details.php
+++ details.php
@@ -2,6 +2,16 @@
 
 include('config/db_connect');
 
+if (isset($_POST['delete'])) {
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+    $sql = "DELETE FROM users WHERE id = $id_to_delete";
+    if (mysqli_query($conn, $sql)) {
+        header('Location: index.php');
+    } else {
+        echo 'query error: ' . mysqli_error($conn);
+    }
+}
+
 if (isset($_GET['id'])) {
     $id = mysqli_real_escape_string($conn, $_GET['id']);
     $sql = "SELECT * FROM users WHERE id = $id";
@@ -10,7 +20,6 @@ if (isset($_GET['id'])) {
 
     mysqli_free_result($result);
     mysqli_close($conn);
-
 }
 
 ?>
@@ -18,28 +27,32 @@ if (isset($_GET['id'])) {
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <link href="styles.css" rel="stylesheet">
-<head>
-<?php include('templates/header.php'); ?>
+    <meta charset="utf-8">
+    <link href="styles.css" rel="stylesheet">
+    <head>
+        <?php include('templates/header.php'); ?>
 
-    <div>
-        <?php if($user): ?>
-            <h4><?php echo htmlspecialchars($user['title']);?></h4>
-                <p>Created by: <?php echo htmlspecialchars($user['email']);?></p>
-                <p><?php echo date($user['created_at']);?></p>
+        <div>
+            <?php if ($user): ?>
+                <h4><?php echo htmlspecialchars($user['title']); ?></h4>
+                <p>Created by: <?php echo htmlspecialchars($user['email']); ?></p>
+                <p><?php echo date($user['created_at']); ?></p>
                 <h5>Interests:</h5>
-                <p><?php echo htmlspecialchars($user['interests']);?></p>
+                <p><?php echo htmlspecialchars($user['interests']); ?></p>
+                <button onclick="history.go(-1);">Back</button>
+                <!--This form is needed for deleting Cards, don't worry about in because in it hidden -->
+                <form action="details.php" method="POST">
+                    <input type="hidden" name="id_to_delete" value="<?php echo $user['id']; ?>">
+                    <input type="submit" name="delete" value="Delete">
+                </form>
+            <?php else: ?>
+                <!--this is an error if somehow you'll direct to non existing User card-->
+                <h5>There is no user under such ID</h5>
+            <?php endif; ?>
 
-        <?php else: ?>
-            <!--this is an error if somehow you'll direct to non existing User card-->
-            <h5>There is no user under such ID</h5>
-        <?php endif; ?>
 
-        <button onclick="history.go(-1);">Back </button>
+        </div>
 
-    </div>
-
-<?php include('templates/footer.php'); ?>
+        <?php include('templates/footer.php'); ?>
 
 </html>
