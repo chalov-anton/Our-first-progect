9505db37a4ac80e7cfbb8f5dd8be4d01c4498b84
Sm-Alex <smirnova.shusha@gmail.com>
2019-12-10 20:49:31 +0200

    Добавлена подсветка Home page

--- templates/header.php
+++ templates/header.php
@@ -1,18 +1,17 @@
 <head>
 	<meta charset="UTF-8">
-  <title></title>
-  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+	<title></title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 </head>
-<body>
 
-	<nav class="main-nav">
-    <ul class="nav-list">
-      <li><a href="index.php">Home Page</a></li>
-      <li><a href="/add.php" class="current">Add</a></li>
-    </ul>
-  </nav>
+<nav class="main-nav">
+	<ul class="nav-list">
+		<li><a href="index.php" class="current">Home Page</a></li>
+		<li><a href="/add.php">Add</a></li>
+	</ul>
+</nav>
 
- 
 
-    
+
+
 
