e115f76b20f21e8d3d02a01652e8b9b5f57d587e
Sm-Alex <smirnova.shusha@gmail.com>
2019-12-11 07:06:31 +0200

    soc icon added-not complete, links needed

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,14 @@
 <footer>
-    <div>Copyright 2019</div>
+	<div class="soc">
+   <a target="_blank" href="your_url_here"><i class="fa fa-2x fa-instagram"></i></a>
+   <a target="_blank" href="your_url_here"><i class="fa fa-2x fa-facebook-square"></i></a>
+   <a target="_blank" href="your_url_here"><i class="fa fa-2x fa-github"></i>
+  </div>
+
+  <div class="copyright-info">Copyright 2019</div>
+
 </footer>
-</body>
+
+
 
 
