a59286c89eb89ba7330fce9dfcd10bb594074d2e
Sm-Alex <smirnova.shusha@gmail.com>
2019-12-11 20:24:38 +0200

    добавлены линки к соц. сетям. fb пока ведет на главную

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,11 @@
 <footer>
 	<div class="soc">
-   <a target="_blank" href="your_url_here"><i class="fa fa-2x fa-instagram"></i></a>
-   <a target="_blank" href="your_url_here"><i class="fa fa-2x fa-facebook-square"></i></a>
-   <a target="_blank" href="your_url_here"><i class="fa fa-2x fa-github"></i>
+   <a target="_blank" href="https://www.instagram.com/chalov_anton/"><i class="fa fa-2x fa-instagram"></i></a>
+   <a target="_blank" href="https://www.facebook.com/"><i class="fa fa-2x fa-facebook-square"></i></a>
+   <a target="_blank" href="https://github.com/chalov-anton/Our-first-project"><i class="fa fa-2x fa-github"></i>
   </div>
 
-  <div class="copyright-info">Copyright 2019</div>
+  <div class="copyright-info"> Copyright 2019</div>
 
 </footer>
 
